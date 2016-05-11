1e5429d50a72782cf910f0c5820c20d510bc2a9d
linzheshang <linzheshang@gmail.com>
2016-05-11 14:58:11 +0800

    change folder

--- RunGrinder.py
+++ RunGrinder.py
@@ -1,16 +1,18 @@
 __author__ = 'minosniu'
 import sys
 import os
+import uuid
+import shutil
 
-path_root = 'D:\\S1_lateral_reaching'
+path_root = 'C:\\shanglinzhe\\bs\\program\\py1\\S2_L_forward_reaching'
 
 expt = 'FES_reaching'
-date = '20150909'
+date = '20150929'
 analyst = 'zcwaxs'
 addr = 'mongodb://localhost:27017/'
-patient = 'YJZ'
+patient = 'CXM'
 side = 'left'
-movement = 'lateral_reaching'
+movement = 'forward_reaching'
 
 if __name__ == '__main__':
     path = path_root
@@ -18,8 +20,12 @@ if __name__ == '__main__':
     for f in files:
         fullname = os.path.abspath(os.path.join(path, f))
         print('Processing "%s"' % (f))
-        print('python Grinder.py "%s" "%s" "%s" "%s" "%s" "%s" "%s" "%s"' % 
-            (fullname, expt, date, analyst, addr, patient, side,movement))
-        os.system('python Grinder.py "%s" "%s" "%s" "%s" "%s" "%s" "%s" "%s"' % 
-            (fullname, expt, date, analyst, addr, patient, side,movement))
+        u = str(uuid.uuid1())
+        os.mkdir('./DataForS2/'+u)
+        shutil.copyfile(fullname, './DataForS2/'+u+'/'+f)
+        print('python Grinder.py "%s" "%s" "%s" "%s" "%s" "%s" "%s" "%s" "%s"' % 
+            ('./DataForS2/'+u+'/'+f, expt, date, analyst, addr, patient, side, movement, u))
+        os.system('python Grinder.py "%s" "%s" "%s" "%s" "%s" "%s" "%s" "%s" "%s"' % 
+          ('./DataForS2/'+u+'/'+f, expt, date, analyst, addr, patient, side, movement, u))
+
 
